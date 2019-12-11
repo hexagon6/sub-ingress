75f5b47aba68edf1eae769171f23933f8f35d024
Hexagon Six <hexagon6@fet.li>
2019-12-11 03:35:48 +0100

    test: server start

--- tests/test1.mjs
+++ tests/test1.mjs
@@ -1,4 +1,11 @@
 import test from 'ava'
+
 import ingress from '../ingress.mjs'
 
 test('should not fail', t => t.true(true))
+
+test.before(async t => {
+  ingress()
+})
+
+test('ingress should start', t => t.true(true))
