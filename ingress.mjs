f3672ef1fe301c0fc47bf60164e6ddf6939e585b
Hexagon Six <hexagon6@fet.li>
2019-12-11 03:03:11 +0100

    chore(cleanup): we stream by default, no more options

--- ingress.mjs
+++ ingress.mjs
@@ -4,17 +4,12 @@ import configure from "./configure.mjs";
 import { assignTargetDomain, reverseProxy } from "./handlers.mjs";
 
 const PORT = process.env.PORT || 3000;
-const NO_STREAMING = process.env.NO_STREAMING || 0;
 
 const domainList = configure();
 
 //  GET  HEAD  PATCH  OPTIONS  CONNECT  DELETE  TRACE  POST  PUT
 const start = () =>
   polka()
-    .use((req, _, next) => {
-      req.config = { noStreaming: NO_STREAMING };
-      next();
-    })
     .use(assignTargetDomain(domainList))
     .get("/*", reverseProxy)
     .head("/*", reverseProxy)
