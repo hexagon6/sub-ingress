b076475b2d5954a9bd1359daa91b92d497ea939b
Hexagon Six <hexagon6@fet.li>
2020-01-09 11:26:42 +0100

    feature(handlers): pass request body to axios request

--- handlers.mjs
+++ handlers.mjs
@@ -43,66 +43,74 @@ export const reverseProxy = (req, res) => {
   const { domain, target, method, originalUrl } = req
   logRP('📨')(`received request [${method}] on ${originalUrl}`)
   if (target) {
-    const newUrl = `http://${target}${originalUrl}`
-    logRP('➡️')(`redirecting "${domain}" -> (${target})`)
-    logRP('🎯')(`redirect to ${newUrl} with method: [${method}]`)
-    axios({
-      method,
-      headers: req.headers,
-      url: newUrl,
-      responseType: 'stream',
+    let body = []
+    req.on('data', chunk => {
+      body.push(chunk)
     })
-      .then(backend => {
-        const {
-          status,
-          headers,
-          request: {
-            res: { rawHeaders },
-          },
-        } = backend
-        logRP('⬅️')(
-          `received answer from backend with status [${status}] and headers [${Object.keys(
-            headers
-          )}]`
-        )
-        logRP('📏')(`content-length of: ${headers['content-length']}`)
-        res.statusCode = status
-
-        send(res, 206, backend.data, {
-          ...transformHeaders(rawHeaders),
-        })
+    req.on('end', () => {
+      const data = body.join('')
+      const newUrl = `http://${target}${originalUrl}`
+      logRP('➡️')(`redirecting "${domain}" -> (${target})`)
+      logRP('🎯')(`redirect to ${newUrl} with method: [${method}]`)
+      axios({
+        method,
+        headers: req.headers,
+        url: newUrl,
+        responseType: 'stream',
+        data,
       })
-      .catch(error => {
-        if (error.response) {
-          logRP('🤭')(
-            `backend responded with [${error.response.status}], I give it back to frontend`
+        .then(backend => {
+          const {
+            status,
+            headers,
+            request: {
+              res: { rawHeaders },
+            },
+          } = backend
+          logRP('⬅️')(
+            `received answer from backend with status [${status}] and headers [${Object.keys(
+              headers
+            )}]`
           )
-          res.statusCode = error.response.status
-          res.end(error.response.data)
-        } else {
-          if (error.isAxiosError) {
-            console.error('axios is to blame')
-          }
-          const { code, address, port, syscall } = error
-          if (code === 'ECONNREFUSED') {
-            logRP('⛔')(
-              `Connection refused to [${address}:${port}] on ${syscall}`
+          logRP('📏')(`content-length of: ${headers['content-length']}`)
+          res.statusCode = status
+
+          send(res, 206, backend.data, {
+            ...transformHeaders(rawHeaders),
+          })
+        })
+        .catch(error => {
+          if (error.response) {
+            logRP('🤭')(
+              `backend responded with [${error.response.status}], I give it back to frontend`
             )
-            res.statusCode = 502
-            res.end('Bad Gateway')
-          } else if (code === 'ECONNRESET') {
-            logRP('❌')(`Connection reset`)
-            res.statusCode = 503
-            res.end('Service Unavailable')
+            res.statusCode = error.response.status
+            res.end(error.response.data)
           } else {
-            logRP('🚨')(`Fetching failed with: `)
-            console.error(error)
-            logRP('💩')(`500 Internal Server Error`)
-            res.statusCode = 500
-            res.end('Internal Server Error')
+            if (error.isAxiosError) {
+              console.error('axios is to blame')
+            }
+            const { code, address, port, syscall } = error
+            if (code === 'ECONNREFUSED') {
+              logRP('⛔')(
+                `Connection refused to [${address}:${port}] on ${syscall}`
+              )
+              res.statusCode = 502
+              res.end('Bad Gateway')
+            } else if (code === 'ECONNRESET') {
+              logRP('❌')(`Connection reset`)
+              res.statusCode = 503
+              res.end('Service Unavailable')
+            } else {
+              logRP('🚨')(`Fetching failed with: `)
+              console.error(error)
+              logRP('💩')(`500 Internal Server Error`)
+              res.statusCode = 500
+              res.end('Internal Server Error')
+            }
           }
-        }
-      })
+        })
+    })
   } else {
     res.statusCode = 421
     res.end('Misdirected request, no such domain configured')
